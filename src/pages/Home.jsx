08aeba04af130545dc15878aa0f940f37e5b01ee
Toufique18 <toufiqueeamam7@gmail.com>
2024-04-27 04:09:24 +0600

    banner complete and commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Banner from "../components/Banner/Banner";
 
 const Home = () => {
     return (
-        <div>
-            <h1>gcfhgjhkj</h1>
+        <div className="relative container mx-auto lg:px-20 px-5 py-5">
+            <Banner></Banner>
+            <div className="absolute  top-1/2">
+                <h1>gcfhgjhkj</h1>
+            </div>
         </div>
     );
 };
